f1d2825e7e3c34b980847aef1405afecb35f9d4d
Karen Calderon R <Karen Calderon R@LAPTOP-QEL8MMUP.mshome.net>
2020-10-26 08:32:04 -0500

    proyecto gestion de ordenes despacho

--- farmaceuticaLaJaveriana-ejb/src/main/java/com/javeriana/ejb/dao/AbstractDao.java
+++ farmaceuticaLaJaveriana-ejb/src/main/java/com/javeriana/ejb/dao/AbstractDao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.javeriana.dao;
+package com.javeriana.ejb.dao;
 
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
